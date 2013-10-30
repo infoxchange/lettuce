82d31189fea4de400441687ad825bab6bb6e1129
Danielle Madeley <danielle@madeley.id.au>
2013-10-30 21:41:16 +1100

    Step to load fixtures with loaddata
    Code originally by Alexey Kotlyarov <alexey@infoxchange.net.au>

--- lettuce/django/steps/__init__.py
+++ lettuce/django/steps/__init__.py
@@ -1,3 +1,11 @@
 """
 Step definitions for use with Django.
 """
+
+from lettuce import step
+from django.core.management import call_command
+
+
+@step(r'Fixtures from "([^"]+)" are loaded')
+def fixtures_loaded(step, fixtures):
+    call_command('loaddata', fixtures, interactive=False)
